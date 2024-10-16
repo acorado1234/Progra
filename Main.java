69ed5794976396cbdc5cd8cdff536e3bada1331a
ignacioaris <arisignacio90@gmail.com>
2024-10-15 22:01:30 -0600

    corrección de sintaxis Main.java

--- Main.java
+++ Main.java
@@ -4,31 +4,75 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-//Funciones utilizadas en todo el codigo para gestionar un control ordenado de los usuarios 
 
 public class Main {
 
-  /**
-   * Lista de usuarios que participan en el sistema.
-   * Contiene instancias de la clase {@link User}, que puede incluir tanto estudiantes como tutores.
-   */
     private static ArrayList<User> users = new ArrayList<>();
-   /**
-    * Lista de actividades académicas disponibles en el sistema.
-    * Contiene instancias de la clase {@link AcademicActivity}, que representan diferentes actividades académicas.
-    */
     private static ArrayList<AcademicActivity> activities = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
+    private static final String CSV_FILE = "users.csv";
 
-   /**
-    * Objeto {@link Scanner} utilizado para leer entradas desde la consola.
-    * Se utiliza en varios métodos para recibir entradas del usuario.
-    */
     public static void main(String[] args) {
+       
+        while (true) {
+            System.out.println("\n--- Sistema de Tutorías ---");
+            System.out.println("1. Iniciar sesión");
+            System.out.println("2. Registrarse");
+            System.out.println("3. Ingresar usuarios (Estudiantes/Tutores)");
+            System.out.println("4. Ver historial de actividades");
+            System.out.println("5. Encontrar coincidencias");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+        
+            int option = scanner.nextInt();
+            scanner.nextLine(); 
+        
+            switch (option) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    register();
+                    break;
+                case 3:
+                    enterUsers();
+                    break;
+                case 4:
+                    showActivityHistory();
+                    break;
+                case 5:
+                    findMatches();
+                    break;
+                case 6:
+                    System.out.println("Saliendo del sistema...");
+                    return;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+    }
+
+  
+    private static void login() {
+        System.out.print("Ingrese su email: ");
+        String email = scanner.nextLine();
+        System.out.print("Ingrese su contraseña: ");
+        String password = scanner.nextLine();
+
+        User user = authenticateUser(email, password);
+
+        if (user != null) {
+            System.out.println("Bienvenido " + user.getName());
+            userMenu(user);
+        } else {
+            System.out.println("Credenciales incorrectas.");
+        }
+    }
 
     private static void saveUserToCSV(User user) {
-       try (FileWriter writer = new FileWriter(CSV_FILE, true)) {
-      if (user instanceof Student) {
+        try (FileWriter writer = new FileWriter(CSV_FILE, true)) {
+            System.out.println("Guardando usuario en el archivo CSV...");
+            if (user instanceof Student) {
                 Student student = (Student) user;
                 writer.append(student.getId()).append(",")
                       .append(student.getName()).append(",")
@@ -36,7 +80,7 @@ public class Main {
                       .append("Estudiante").append(",")
                       .append(student.getMajor()).append(",")
                       .append(String.join(";", student.getInterests())).append("\n");
-            }else if (user instanceof Tutor) {
+            } else if (user instanceof Tutor) {
                 Tutor tutor = (Tutor) user;
                 writer.append(tutor.getId()).append(",")
                       .append(tutor.getName()).append(",")
@@ -45,45 +89,117 @@ public class Main {
                       .append(tutor.getSubjectExpertise()).append(",")
                       .append(String.join(";", tutor.getSpecializations())).append("\n");
             }
-          System.out.println("Usuario guardado exitosamente.");
-       }catch (IOException e) {
+            System.out.println("Usuario guardado exitosamente.");
+        } catch (IOException e) {
             System.out.println("Error al guardar el usuario en el archivo CSV: " + e.getMessage());
         }
     }
+    
 
-  /**
-   * Encuentra coincidencias entre estudiantes y tutores en el sistema de emparejamiento.
-   * Dependiendo de la opción seleccionada por el usuario, se buscarán coincidencias
-   * para un estudiante o un tutor, basadas en su ID.
-   * 
-   * El método interactúa con el sistema de emparejamiento {@link MatchingSystem} para
-   * agregar estudiantes y tutores, y luego busca coincidencias según la opción seleccionada.
-   * 
-   * Funcionalidad:
-   * 1. Buscar coincidencias para un estudiante basado en su ID.
-   * 2. Buscar coincidencias para un tutor basado en su ID.
-   * 
-   * Proceso:
-   * - Los usuarios se dividen en estudiantes y tutores.
-   * - El sistema solicita al usuario seleccionar una opción y proporciona el ID del estudiante o tutor.
-   * - El sistema encuentra las coincidencias y las muestra en la consola.
-   * 
-   * Excepciones:
-   * - Si no se encuentra un estudiante o tutor con el ID proporcionado, se muestra un mensaje de error.
-   * 
-   * Variables:
-   * - `users`: Lista de usuarios (tutores y estudiantes).
-   * - `scanner`: Objeto para leer la entrada del usuario desde la consola.
-   * 
-   * Opción seleccionada:
-   * - 1: Buscar coincidencias para un estudiante.
-   * - 2: Buscar coincidencias para un tutor.
-   */
+    private static User authenticateUser(String email, String password) {
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+    private static void register() {
+        System.out.print("Ingrese su ID: ");
+        String id = scanner.nextLine();
+        System.out.print("Ingrese su nombre: ");
+        String name = scanner.nextLine();
+        System.out.print("Ingrese su email: ");
+        String email = scanner.nextLine();
+        System.out.print("Ingrese su contraseña: ");
+        String password = scanner.nextLine();
+        System.out.print("¿Es estudiante o tutor? (1. Estudiante, 2. Tutor): ");
+        int userType = scanner.nextInt();
+        scanner.nextLine(); 
+
+        if (userType == 1) {
+            System.out.print("Ingrese su carrera: ");
+            String major = scanner.nextLine();
+            String[] interestsArray = scanner.nextLine().split(",");
+            List<String> interests = Arrays.asList(interestsArray);
+            Student student = new Student(id, name, email, password, major, interests);
+            users.add(student);
+            saveUserToCSV(student);
+            System.out.println("Estudiante registrado exitosamente.");
+        } else if (userType == 2) {
+            System.out.print("Ingrese su área de especialización: ");
+            String subjectExpertise = scanner.nextLine();
+            String[] specializationsArray = scanner.nextLine().split(",");
+            List<String> specializations = Arrays.asList(specializationsArray);
+            Tutor tutor = new Tutor(id, name, email, password, subjectExpertise, specializations);
+            users.add(tutor);
+            saveUserToCSV(tutor);
+            System.out.println("Tutor registrado exitosamente.");
+        } else {
+            System.out.println("Opción no válida.");
+        }
+    }
+
+   
+    private static void enterUsers() {
+        System.out.println("\n--- Ingresar Usuarios (Estudiantes/Tutores) ---");
+        System.out.print("¿Cuántos usuarios desea ingresar? ");
+        int numberOfUsers = scanner.nextInt();
+        scanner.nextLine(); 
+
+        for (int i = 0; i < numberOfUsers; i++) {
+            System.out.print("Ingrese su ID: ");
+            String id = scanner.nextLine();
+            System.out.print("Ingrese su nombre: ");
+            String name = scanner.nextLine();
+            System.out.print("Ingrese su email: ");
+            String email = scanner.nextLine();
+            System.out.print("Ingrese su contraseña: ");
+            String password = scanner.nextLine();
+            System.out.print("¿Es estudiante o tutor? (1. Estudiante, 2. Tutor): ");
+            int userType = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (userType == 1) {
+                System.out.print("Ingrese su carrera: ");
+                String major = scanner.nextLine();
+                String[] interestsArray = scanner.nextLine().split(",");
+                List<String> interests = Arrays.asList(interestsArray);
+                users.add(new Student(id, name, email, password, major,interests));
+                System.out.println("Estudiante registrado exitosamente.");
+            } else if (userType == 2) {
+                System.out.print("Ingrese su área de especialización: ");
+                String subjectExpertise = scanner.nextLine();
+                String[] specializationsArray = scanner.nextLine().split(",");
+                List<String> specializations = Arrays.asList(specializationsArray);
+                users.add(new Tutor(id, name, email, password, subjectExpertise,specializations));
+                System.out.println("Tutor registrado exitosamente.");
+            } else {
+                System.out.println("Opción no válida.");
+            }
+        }
+    }
+
+   
+    private static void showActivityHistory() {
+        System.out.println("\n--- Historial de Actividades ---");
+        for (AcademicActivity activity : activities) {
+            System.out.println(activity.toString());
+        }
+    }
+
+    
+    private static void userMenu(User user) {
+        System.out.println("\n--- Bienvenido, " + user.getName() + " ---");
+       
+    }
+
     private static void findMatches() {
         MatchingSystem matchingSystem = new MatchingSystem();
-    
-        // Agregar estudiantes y tutores al sistema de emparejamiento
-        for (User user : users) {
+        
+        for (User  user : users) {
             if (user instanceof Student) {
                 matchingSystem.addStudent((Student) user);
             } else if (user instanceof Tutor) {
@@ -91,23 +207,20 @@ public class Main {
             }
         }
     
-        // Menú para encontrar coincidencias
         System.out.println("\n--- Encontrar coincidencias ---");
         System.out.println("1. Encontrar coincidencias para un estudiante");
         System.out.println("2. Encontrar coincidencias para un tutor");
         System.out.print("Seleccione una opción: ");
-
+    
         int option = scanner.nextInt();
         scanner.nextLine(); 
-      //Utilizamos funcion scanner para ver si hay coincidencias entre estudiante o tutor 
-        
+    
         switch (option) {
             case 1:
-                // Buscar coincidencias para un estudiante
                 System.out.print("Ingrese el ID del estudiante: ");
                 String studentId = scanner.nextLine();
                 Student student = null;
-                for (User user : users) {
+                for (User  user : users) {
                     if (user instanceof Student && user.getId().equals(studentId)) {
                         student = (Student) user;
                         break;
@@ -124,17 +237,15 @@ public class Main {
                 }
                 break;
             case 2:
-                // Buscar coincidencias para un tutor
                 System.out.print("Ingrese el ID del tutor: ");
                 String tutorId = scanner.nextLine();
                 Tutor tutor = null;
-                for (User user : users) {
+                for (User  user : users) {
                     if (user instanceof Tutor && user.getId().equals(tutorId)) {
                         tutor = (Tutor) user;
                         break;
                     }
                 }
-            //Utilizamos una instancia para ver si el id del tutor es igual al ingresado 
                 if (tutor != null) {
                     List<Student> matches = matchingSystem.findMatchesForTutor(tutor);
                     System.out.println("Coincidencias encontradas para el tutor " + tutor.getName() + ":");
@@ -147,80 +258,8 @@ public class Main {
                 break;
             default:
                 System.out.println("Opción no válida.");
-                break;
         }
-
     }
 
-    /**
-     * Permite ingresar nuevos usuarios al sistema, tanto estudiantes como tutores.
-     * 
-     * Se solicita al usuario que ingrese la cantidad de usuarios a registrar, luego para cada usuario
-     * se capturan los datos básicos (ID, nombre, email, contraseña) y dependiendo de si es estudiante
-     * o tutor, se capturan datos adicionales específicos.
-     * 
-     * Estudiantes:
-     * - Se solicita la carrera y los intereses (separados por comas).
-     * 
-     * Tutores:
-     * - Se solicita el área de especialización y las especializaciones adicionales (separadas por comas).
-     * 
-     * Manejo de errores:
-     * - Si el usuario ingresa una opción no válida para el tipo de usuario, se muestra un mensaje de error.
-     */
-    private static void enterUsers() {
-        System.out.println("\n--- Ingresar Usuarios (Estudiantes/Tutores) ---");
-        System.out.print("¿Cuántos usuarios desea ingresar? ");
-        int numberOfUsers = scanner.nextInt();
-        scanner.nextLine(); 
-      // Cuenta cuantos usuarios queremos ingresar si 1 o mas
-    
-        for (int i = 0; i < numberOfUsers; i++) {
-            System.out.print("Ingrese su ID: ");
-            String id = scanner.nextLine();
-            System.out.print("Ingrese su nombre: ");
-            String name = scanner.nextLine();
-            System.out.print("Ingrese su email: ");
-            String email = scanner.nextLine();
-            System.out.print("Ingrese su contraseña: ");
-            String password = scanner.nextLine();
-          //Si el entero i = 0 solicitamos todos los datos para verificar que tipo de cuenta tiene si de estudiante o tutor 
-
-            System.out.print("¿Es estudiante o tutor? (1. Estudiante, 2. Tutor): ");
-            int userType = scanner.nextInt();
-            scanner.nextLine(); 
-            
-            if (userType == 1) {
-                System.out.print("Ingrese su carrera: ");
-                String major = scanner.nextLine();
-                System.out.print("Ingrese sus intereses (separados por comas): ");
-                String[] interestsArray = scanner.nextLine().split(",");
-              //Arreglo para llamar la funcion interes de estudiante
-                List<String> interests = Arrays.asList(interestsArray);
-                users.add(new Student(id, name, email, password, major, interests));
-                System.out.println("Estudiante registrado exitosamente.");
-              //Si opcion 1 cumple con todos los datos sin niguna falta se registra usuario 
-            else if (userType == 2) {
-                System.out.print("Ingrese su área de especialización: ");
-                String subjectExpertise = scanner.nextLine();
-                System.out.print("Ingrese sus especializaciones (separadas por comas): ");
-                String[] specializationsArray = scanner.nextLine().split(",");
-                List<String> specializations = Arrays.asList(specializationsArray);
-              //Llamamos el arreglo de especialidad de la clase tutor como una lista asi podemos agregar al tutor con toda su informacion
-                users.add(new Tutor(id, name, email, password, subjectExpertise, specializations));
-                System.out.println("Tutor registrado exitosamente.");
-            else {
-                System.out.println("Opción no válida.");
-              // Opcion final para agregar un nuevo tutor y registrarlo en la db
-}
 
 }
-
-}
-    }
-}       
-    
-
-
-
-
