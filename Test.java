66da8906b1cd62c78173912dc66ffdadc76d0329
ignacioaris <arisignacio90@gmail.com>
2024-10-22 09:29:21 -0600

    agregar constructor a Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Test{
-int respuestasCorrectas;
+  private ArrayList<Question> preguntas;
+  private int respuestasCorrectas;
+
+  public Test(){
+    this.preguntas = new ArrayList<>();
+    this.respuestasCorrectas = 0;
+  }
+  public void agregarPregunta(Question pregunta) {
+    preguntas.add(pregunta);
+    }
 }
