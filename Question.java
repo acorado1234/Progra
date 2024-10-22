55c5e9e364fcf77d39d183c194e21de05ebfb531
ignacioaris <arisignacio90@gmail.com>
2024-10-22 09:31:17 -0600

    constructor y metodo nuevo Question.java

--- Question.java
+++ Question.java
@@ -5,4 +5,19 @@ public class Question{
   private ArrayList<String> opciones;
   private int respuestaCorrecta;
 
+  public Question(String enunciado, ArrayList<String> opciones, int respuestaCorrecta) {
+        this.enunciado = enunciado;
+        this.opciones = opciones;
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+
+    /**
+     * Muestra la pregunta y las opciones.
+     */
+    public void mostrarPregunta() {
+        System.out.println(enunciado);
+        for (int i = 0; i < opciones.size(); i++) {
+            System.out.println((i + 1) + ". " + opciones.get(i));
+        }
+    }
 }
