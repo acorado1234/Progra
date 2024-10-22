c69a315579cbdc89337d23e69dde30b04057c901
ignacioaris <arisignacio90@gmail.com>
2024-10-22 09:45:48 -0600

    comentarios java.Doc Question.java

--- Question.java
+++ Question.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
-
+/**
+ * Clase que representa una pregunta de opción múltiples
+ */
 public class Question{
   private String enunciado;
   private ArrayList<String> opciones;
   private int respuestaCorrecta;
 
+    
+  /**
+    * Constructor de la clase Pregunta.
+    * @param enunciado El enunciado de la pregunta.
+    * @param opciones Las posibles respuestas de opción múltiple.
+    * @param respuestaCorrecta El índice de la respuesta correcta.
+    */
   public Question(String enunciado, ArrayList<String> opciones, int respuestaCorrecta) {
         this.enunciado = enunciado;
         this.opciones = opciones;
@@ -20,7 +29,11 @@ public class Question{
             System.out.println((i + 1) + ". " + opciones.get(i));
         }
     }
-
+    /**
+     * Verifica si la respuesta proporcionada es correcta.
+     * @param respuesta El índice de la respuesta seleccionada por el alumno.
+     * @return true si la respuesta es correcta, false en caso contrario.
+     */
   public boolean esRespuestaCorrecta(int respuesta) {
          if (respuesta == this.respuestaCorrecta) {
              return true;
