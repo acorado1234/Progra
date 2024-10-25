4e3035883a3df51e4060355bd0a23555a2d02d81
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-10-24 20:30:41 -0600

    Update Test.java
    Complemente con este metodo que es el que permite cargar preguntas desde un csv conteniendo enunciado de la pregunta, cuatro respuestas y el num de resps. correctas. Con sus debidos javadocs

--- Test.java
+++ Test.java
@@ -33,4 +33,32 @@ public class Test{
   public int getRespuestasCorrectas(){
         return this.respuestasCorrectas;
     }
+  /**
+  *Carga preguntas desde el CSV y las agrega a un examen
+  *@param void nombreArchivo nombre del csv
+  *\
+  public void cargarPreguntasDesdeCSV(String nombreArchivo) {
+
+    String linea; 
+    try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+      br.readLine();
+
+      while ((linea = br.readLine()) != null) {
+        ArrayList<String> opciones = new ArrayList<>();
+        String[] datos = linea.split(",");
+        String enunciado = datos[0];
+        opciones.add(datos[1]);
+        opciones.add(datos[2]);
+        opciones.add(datos[3]);
+        opciones.add(datos[4]);
+        int respuestaCorrecta = Integer.parseInt(datos[5]);
+        Question pregunta = new Question(enunciado, opciones, respuestaCorrecta);
+
+        preguntas.add(pregunta);
+      }
+    } catch(IOException e){
+      System.out.println("Error al leer el archivo: " + e.getMessage());
+    }
+  }
+  
 }
