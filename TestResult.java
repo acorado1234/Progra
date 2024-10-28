2a389502fa8e1a6f915686dba8452957d4127b37
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-10-28 15:46:03 -0600

    Create TestResult.java
    Declaré la clase definiendo los atributos alumno, respuestas correctas y total de preguntas para almacenar la información del examen y del alumno.

---
+++ TestResult.java
@@ -0,0 +1,7 @@
+/**
+* Es la clase que calcula y almacena los resultados de un examen 
+  */
+public class TestResult {
+  private Student alumno;
+  private int respuestasCorrectas;
+  private int totalPreguntas; 
