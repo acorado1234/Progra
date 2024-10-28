290161c399a66c6211a235b34a734e983c97cc62
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-10-28 15:57:27 -0600

    Update TestResult.java
    creé el constructor de TestResult para iniciar los atributos previamente señalados

--- TestResult.java
+++ TestResult.java
@@ -5,3 +5,15 @@ public class TestResult {
   private Student alumno;
   private int respuestasCorrectas;
   private int totalPreguntas; 
+/**
+* Constructor de la clase
+ *@param alumno el alumno que esta realizando el examen
+ *@param respuestasCorrectas el num de respuestas correctas
+ *@param totalPreguntas el num de preguntas en el examen 
+ */
+public TestResult(Student alumno, int respuestasCorrectas, int totalPreguntas) {
+ this.alumno = alumno;
+ this.respuestasCorrectas = respuestasCorrectas;
+ this.totalPreguntas = totalPreguntas;
+}
+ 
